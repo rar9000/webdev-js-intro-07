21feb2430fe4d2dd607e96d62a0ae5e95b327de5
rar9000 <clavius@Clavius.local>
2025-01-18 18:46:08 -0500

    completed

--- js/assignment.js
+++ js/assignment.js
@@ -6,3 +6,26 @@ const checkIfEvenElement = document.getElementById("check-if-even");
 const lostNumbersElement = document.getElementById("lost-numbers");
 
 const lostNumbers = [4, 8, 15, 16, 23, 42];
+
+
+function returnTheRemainder(num1, num2) {
+    remainderElement.innerText = (num1 % num2);
+} 
+
+function checkIfEven(number) {
+    checkIfEvenElement.innerText = (number % 2 === 0);
+}
+
+function getTheFourthElement(array) {
+    lostNumbersElement.innerText = (array[3])
+}
+
+function render() {
+    returnTheRemainder(5, 4);
+    checkIfEven(47);
+    getTheFourthElement(lostNumbers);
+}
+
+submissionBtn.addEventListener("click", function () {
+    render();
+});
--- js/index.js
+++ js/index.js
@@ -2,3 +2,39 @@
 // that are not declared
 "use strict"
 
+/*
+function defuction() {
+    console.log('weofvmsgghracmad');
+}
+
+defuction();
+
+
+
+function splice(halfWord1, halfWord2) {
+    return halfWord1 + halfWord2;
+}
+
+wholeword = splice('Wo', 'rd');
+console.log(wholeword);
+
+
+
+function wholeword(word) {
+    console.log(word)
+}
+
+wholeword('whole');
+let word = wholeword('word');
+console.log(word);
+
+
+
+function day(today = 'a day') {
+    console.log('Today is ' + today);
+}
+
+day();
+day('Tuesday');
+
+*/
